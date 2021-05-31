6188e29ecfe111eb791a72ccb95d13131abaf710
Kirill Mironov <vetrokm@gmail.com>
2021-05-31 15:41:41 +0300

    test(core): add unit tests for session token extractor

--- core/src/app/error.rs
+++ core/src/app/error.rs
@@ -2,7 +2,7 @@ use actix_web::http::StatusCode;
 use actix_web::ResponseError;
 use thiserror::Error;
 
-#[derive(Debug, Error)]
+#[derive(Debug, Error, Eq, PartialEq)]
 #[non_exhaustive]
 pub enum SessionTokenExtractorError {
     #[error("No session token present in cookie")]
