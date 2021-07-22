3975ba260c8a40cb3d59bfcaa98aad55e7006e98
Kirill Mironov <vetrokm@gmail.com>
2021-07-22 16:51:23 +0300

    Change migrations

--- migrations/20210719200826_update_users.up.sql
+++ migrations/20210719200826_update_users.up.sql
@@ -1,6 +1,6 @@
 -- Add up migration script here
 ALTER TABLE users
-    ADD COLUMN username varchar NOT NULL,
+    ADD COLUMN username varchar NOT NULL DEFAULT '',
     ADD COLUMN bio      varchar,
     ADD COLUMN avatar   varchar,
     ADD COLUMN work     varchar;
