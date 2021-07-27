d41bfeb99fa8defa5208e33694a05deedab8fcb2
Kirill Mironov <vetrokm@gmail.com>
2021-07-27 22:31:36 +0300

    Use trigram extension in migration (#36)

--- migrations/20210721125045_cards_indices.up.sql
+++ migrations/20210721125045_cards_indices.up.sql
@@ -1,4 +1,6 @@
 -- Add up migration script here
+CREATE EXTENSION IF NOT EXISTS "pg_trgm";
+
 CREATE OR REPLACE FUNCTION jsonb_to_tsvector_multilang(jsonb, jsonb) RETURNS
     TSVECTOR AS $$
 SELECT jsonb_to_tsvector('english', $1, $2) ||
